c4a8a8a41618271efc683497ff7a27bb5103e31b
NamanGupta06 <namangupta6415@gmail.com>
2025-02-24 01:36:08 +0530

    Update Day_4.py

--- Day_4.py
+++ Day_4.py
@@ -3,7 +3,7 @@ import numpy as np
 import altair as alt
 import pandas as pd
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 
 st.header("st.write()")
 
